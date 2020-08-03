a8d57c294d06e6261fdd324f06db3dd171c36472
Hung <s3715272@rmit.edu.vn>
2020-08-03 16:34:33 +1000

    completed backend

--- demo/src/main/java/com/example/demo/services/ProjectService.java
+++ demo/src/main/java/com/example/demo/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.Project;
+import com.example.demo.exceptions.ProjectIdException;
 import com.example.demo.repo.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,41 @@ public class ProjectService {
     private ProjectRepo projectRepo;
 
     public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepo.save(project);
 
-        //Logic
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '"
+                    + project.getProjectIdentifier().toUpperCase()
+                    + "' already exists");
+        }
+    }
 
-        return projectRepo.save(project);
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepo.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"
+                    + projectId
+                    + "' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProject(){
+        return projectRepo.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepo.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' not found");
+        }
+
+        projectRepo.delete(project);
     }
 }
