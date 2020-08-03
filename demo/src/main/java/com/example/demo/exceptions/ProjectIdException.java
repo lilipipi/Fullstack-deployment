dfa2f2507842eb62b15982d76cf887faff1f8de4
Hung <s3715272@rmit.edu.vn>
2020-08-03 23:15:51 +1000

    added suppresswarning annotations

--- demo/src/main/java/com/example/demo/exceptions/ProjectIdException.java
+++ demo/src/main/java/com/example/demo/exceptions/ProjectIdException.java
@@ -3,8 +3,9 @@ package com.example.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ProjectIdException extends RuntimeException{
+public class ProjectIdException extends RuntimeException {
 
     public ProjectIdException(String s) {
         super(s);
--- demo/src/main/java/com/example/demo/web/ProjectController.java
+++ demo/src/main/java/com/example/demo/web/ProjectController.java
@@ -7,13 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -29,14 +25,15 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap != null) return errorMap;
+        if (errorMap != null)
+            return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 
     @GetMapping("/{projectId}")
-    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
 
         Project project = projectService.findProjectByIdentifier(projectId);
 
@@ -44,12 +41,14 @@ public class ProjectController {
     }
 
     @GetMapping("/all")
-    public Iterable<Project> getAllProjects(){return projectService.findAllProject();}
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProject();
+    }
 
     @DeleteMapping("/{projectId}")
-    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
-       projectService.deleteProjectByIdentifier(projectId);
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
 
-       return new ResponseEntity<String>("Project with ID '" + projectId + "' deleted successfully", HttpStatus.OK);
+        return new ResponseEntity<String>("Project with ID '" + projectId + "' deleted successfully", HttpStatus.OK);
     }
 }
