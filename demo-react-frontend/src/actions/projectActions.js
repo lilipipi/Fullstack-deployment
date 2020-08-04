7b190884950a6a1b896c94b0f98725c231441b68
Hung <s3715272@rmit.edu.vn>
2020-08-04 22:27:23 +1000

    rename unsafe life cycle hooks

--- demo-react-frontend/src/actions/projectActions.js
+++ demo-react-frontend/src/actions/projectActions.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
-    let url = "http://localhost:8080/api/project";
-    const res = await axios.post(url, project);
+    const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
@@ -15,10 +14,17 @@ export const createProject = (project, history) => async (dispatch) => {
 };
 
 export const getProjects = () => async (dispatch) => {
-  let url = "http://localhost:8080/api/project/all";
-  const res = await axios.get(url);
+  const res = await axios.get("http://localhost:8080/api/project/all");
   dispatch({
     type: GET_PROJECTS,
+    payload: res.data,
+  });
+};
+
+export const getProject = (id, history) => async (dispatch) => {
+  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
     payload: res.data,
   });
 };
