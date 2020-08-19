140131fb961b90489b86f7799b5ac86e22e69416
s3607139 <s3607139@student.rmit.edu.au>
2020-08-19 16:49:13 +1000

    Login and Sign up front end
    Basic login and sign up page created
    Not yet connected to backend

--- demo-react-frontend/src/App.js
+++ demo-react-frontend/src/App.js
@@ -8,6 +8,8 @@ import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import Login from "./components/Project/Login"
+import SignUp from"./components/Project/SignUp";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/login.html" component={Login} />
+          <Route exact path="/register.html" component={SignUp} />
         </div>
       </Router>
     </Provider>
---
+++ demo-react-frontend/src/components/Project/SignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Button, Form} from "react-bootstrap";
+
+class SignUp extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Register</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUp;
