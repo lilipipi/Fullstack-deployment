a557c4d5810bc6489ca3a6196253761f1b4f620f
Hung <s3715272@rmit.edu.vn>
2020-08-29 21:42:23 +1000

    added security configs + user to appointment relationships

--- demo/src/main/java/com/example/demo/services/AppointmentService.java
+++ demo/src/main/java/com/example/demo/services/AppointmentService.java
@@ -2,9 +2,12 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Appointment;
 import com.example.demo.domain.AppointmentTaskList;
+import com.example.demo.domain.User;
 import com.example.demo.exceptions.AppointmentIdException;
+import com.example.demo.exceptions.AppointmentNotFoundException;
 import com.example.demo.repo.AppointmentTaskListRepo;
 import com.example.demo.repo.AppointmentRepo;
+import com.example.demo.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,28 @@ public class AppointmentService {
     @Autowired
     private AppointmentTaskListRepo appointmentTaskListRepo;
 
-    public Appointment saveOrUpdateAppointment(Appointment appointment){
+    @Autowired
+    private UserRepo userRepo;
+
+    public Appointment saveOrUpdateAppointment(Appointment appointment, String username){
+
+        if(appointment.getId() != null) {
+            Appointment existingAppointment = appointmentRepo.findByAppointmentIdentifier(appointment.getAppointmentIdentifier());
+
+            if(existingAppointment != null &&(!existingAppointment.getAppointmentOwner().equals(username))){
+                throw new AppointmentNotFoundException("Appointment not found in your account");
+            } else if (existingAppointment == null) {
+                throw new AppointmentNotFoundException("Appointment with ID '" + appointment.getAppointmentIdentifier() + "' cannot be updated because it doesn't exist");
+            }
+        }
+
         try{
+
+            User user = userRepo.findByUsername(username);
+
+            appointment.setUser(user);
+            appointment.setAppointmentOwner(user.getUsername());
+
             String identifier = appointment.getAppointmentIdentifier().toUpperCase();
             appointment.setAppointmentIdentifier(identifier);
 
@@ -42,7 +65,7 @@ public class AppointmentService {
         }
     }
 
-    public Appointment findAppointmentByIdentifier(String appointmentId){
+    public Appointment findAppointmentByIdentifier(String appointmentId, String username){
 
         Appointment appointment = appointmentRepo.findByAppointmentIdentifier(appointmentId.toUpperCase());
 
@@ -50,21 +73,20 @@ public class AppointmentService {
             throw new AppointmentIdException("Appointment ID '" + appointmentId + "' does not exist");
         }
 
+        if(!appointment.getAppointmentOwner().equals(username)){
+            throw new AppointmentNotFoundException("Appointment not found in your account");
+        }
+
         return appointment;
     }
 
-    public Iterable<Appointment> findAllAppointment(){
-        return appointmentRepo.findAll();
+    public Iterable<Appointment> findAllAppointment(String username){
+        return appointmentRepo.findAllByAppointmentOwner(username);
     }
 
-    public void deleteAppointmentByIdentifier(String appointmentId){
-        Appointment appointment = appointmentRepo.findByAppointmentIdentifier(appointmentId.toUpperCase());
+    public void deleteAppointmentByIdentifier(String appointmentId, String username){
 
-        if (appointment == null){
-            throw new AppointmentIdException("Appointment ID '" + appointmentId + "' not found");
-        }
-
-        appointmentRepo.delete(appointment);
+        appointmentRepo.delete(findAppointmentByIdentifier(appointmentId, username));
     }
 
 
