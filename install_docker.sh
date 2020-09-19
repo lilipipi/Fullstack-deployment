4c1a1331c483a3be3140b5a6e7bd0931f30582a1
Hung <s3715272@rmit.edu.vn>
2020-09-19 22:40:04 +1000

    fixed docker image for backend + edit ansible playbook

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set +ex
 
-yum update
-yum install docker
+yum update -y
+yum install docker -y
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
